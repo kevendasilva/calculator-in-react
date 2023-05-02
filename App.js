6c223a24ce4ad32e66575839d11940cc1fe378fb
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-05-01 23:57:05 -0300

    feat: creating layout

--- App.js
+++ App.js
@@ -1,20 +1,143 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.calculator}>
+      <View style={styles.display}>
+        <Text style={styles.currentLine}>333</Text>
+      </View>
+      <View style={styles.keyboard}>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.special}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.special}>+/-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.special}>%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.operator}>÷</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.operator}>x</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.operator}>-</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.operator}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>⟳</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.data}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.key}>
+            <Text style={styles.operator}>=</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  calculator: {
+    backgroundColor: '#22252d',
+    flex: 1
   },
+
+  display: {
+    alignItems: 'flex-end',
+    flex: 2,
+    justifyContent: 'flex-end',
+    padding: 36
+  },
+
+  currentLine: {
+    color: '#f2f2f3',
+    fontSize: 64
+  },
+
+  keyboard: {
+    backgroundColor: '#292d36',
+    borderRadius: 36,
+    flex: 3,
+    padding: 20
+  },
+
+  row: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+
+  key: {
+    alignItems: 'center',
+    backgroundColor: '#272b33',
+    borderRadius: 24,
+    flex: 1,
+    fontSize: 24,
+    justifyContent: 'center',
+    margin: 12,
+    width: 'auto'
+  },
+
+  data: {
+    color: '#f2f2f3',
+    fontSize: 24,
+    fontWeight: '500'
+  },
+
+  special: {
+    color: '#26f6d1',
+    fontSize: 24,
+    fontWeight: '500'
+  },
+
+  operator: {
+    color:  '#f47a7a',
+    fontSize: 24,
+    fontWeight: '500'
+  }
 });
